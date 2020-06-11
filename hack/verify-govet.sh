fed74e2ca881f03eff296a2ac8f50aa04423cb4a
thiagoalessio <talessio@redhat.com>
2020-06-11 13:55:43 +0200

    Replace whitelist/blacklist terminology with allowlist/denylist

--- hack/verify-govet.sh
+++ hack/verify-govet.sh
@@ -10,13 +10,13 @@ trap "cleanup" EXIT
 
 os::golang::verify_go_version
 
-govet_blacklist=( "${OS_GOVET_BLACKLIST[@]-}" )
+govet_denylist=( "${OS_GOVET_DENYLIST[@]-}" )
 
-function govet_blacklist_contains() {
+function govet_denylist_contains() {
 	local text=$1
-	for blacklist_entry in "${govet_blacklist[@]-}"; do
-		if grep -Eqx "${blacklist_entry}" <<<"${text}"; then
-			# the text we got matches this blacklist entry
+	for denylist_entry in "${govet_denylist[@]-}"; do
+		if grep -Eqx "${denylist_entry}" <<<"${text}"; then
+			# the text we got matches this denylist entry
 			return 0
 		fi
 	done
@@ -26,7 +26,7 @@ function govet_blacklist_contains() {
 for test_dir in $(os::util::list_go_src_dirs); do
 	if ! result="$(go tool vet -shadow=false -printfuncs=Info,Infof,Warning,Warningf "${test_dir}" 2>&1)"; then
 		while read -r line; do
-			if ! govet_blacklist_contains "${line}"; then
+			if ! govet_denylist_contains "${line}"; then
 				echo "${line}"
 				FAILURE=true
 			fi
